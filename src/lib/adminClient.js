325a5973876047d45785b397a0c47a46b5867691
John Lloyd Umani <umani.johnlloyd.esplana@gmail.com>
2024-11-11 21:28:24 +0800

    fixing bug server logging out attempt

--- src/lib/adminClient.js
+++ src/lib/adminClient.js
@@ -1,8 +1,18 @@
 // src/lib/adminClient.js
-import { createClient } from '@supabase/supabase-js';
-import { PRIVATE_SUPABASE_URL, PRIVATE_SUPABASE_SERVICE_KEY } from '$env/static/private';
+import { createClient } from "@supabase/supabase-js";
+import {
+  PRIVATE_SUPABASE_URL,
+  PRIVATE_SUPABASE_SERVICE_KEY,
+} from "$env/static/private";
 
-const supabaseUrl = PRIVATE_SUPABASE_URL;
-const supabaseServiceKey = PRIVATE_SUPABASE_SERVICE_KEY;
-
-export const adminClient = createClient(supabaseUrl, supabaseServiceKey);
+export const adminClient = createClient(
+  PRIVATE_SUPABASE_URL,
+  PRIVATE_SUPABASE_SERVICE_KEY,
+  {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  }
+);
